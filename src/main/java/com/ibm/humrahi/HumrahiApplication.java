807250a37cfd9da4a76cdfe83e7399df904679dd
Manoj Behl <manobehl@in.ibm.com>
2020-05-28 20:58:37 +0530

    initial setup

---
+++ src/main/java/com/ibm/humrahi/HumrahiApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.humrahi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumrahiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumrahiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibm/humrahi/HumrahiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.humrahi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumrahiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
